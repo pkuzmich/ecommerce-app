261fed0fddaf91d3e3d52d20a99ee2ed3bfcb2da
Pavel Kuzmich <kuzmich@sovanet.cz>
2025-10-11 22:47:48 +0200

    Theme Mode Toggle

--- components/shared/header/index.tsx
+++ components/shared/header/index.tsx
@@ -3,6 +3,7 @@ import { APP_NAME } from "@/lib/constants";
 import { ShoppingCart, UserIcon } from "lucide-react";
 import Image from "next/image";
 import Link from "next/link";
+import ModeToogle from "./mode-toggle";
 
 const Header = () => {
   return (
@@ -23,6 +24,7 @@ const Header = () => {
           </Link>
         </div>
         <div className="space-x-2">
+          <ModeToogle />
           <Button asChild variant="ghost">
             <Link href="/cart">
               <ShoppingCart /> Cart
