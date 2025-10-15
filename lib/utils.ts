2ef8b66c77e7b7efcfb23a1489590983acf0611b
Pavel Kuzmich <pkuzmich.cz@gmail.com>
2025-10-15 20:32:13 +0200

    Format number with decimal places

--- lib/utils.ts
+++ lib/utils.ts
@@ -9,3 +9,15 @@ export function cn(...inputs: ClassValue[]) {
 export function convertToPlainObject<T>(value: T): T {
   return JSON.parse(JSON.stringify(value));
 }
+
+// Format number with decimal places
+/*
+formatNumberWithDecimal(5) → "5.00"
+formatNumberWithDecimal(5.1) → "5.10"
+formatNumberWithDecimal(5.12) → "5.12"
+
+*/
+export function formatNumberWithDecimal(num: number): string {
+  const [int, decimal] = num.toString().split(".");
+  return decimal ? `${int}.${decimal.padEnd(2, "0")}` : `${int}.00`;
+}
