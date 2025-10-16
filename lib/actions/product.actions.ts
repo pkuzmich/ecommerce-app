afee22396f237752ca39e11a0518da4e62a69e19
Pavel Kuzmich <pkuzmich.cz@gmail.com>
2025-10-16 22:55:38 +0200

    Serverless Environment config

--- lib/actions/product.actions.ts
+++ lib/actions/product.actions.ts
@@ -1,12 +1,10 @@
 "use server";
-import { PrismaClient } from "@/lib/generated/prisma";
+import { prisma } from "@/db/prisma";
 import { convertToPlainObject } from "@/lib/utils";
 import { LATEST_PRODUCTS_LIMIT } from "../constants";
 
 //Get getLatestProducts products
 export async function getLatestProducts() {
-  const prisma = new PrismaClient();
-
   const data = await prisma.product.findMany({
     take: LATEST_PRODUCTS_LIMIT,
     orderBy: {
