d078ee2535c44137bb8e50305b410f9e155feb03
Pavel Kuzmich <pkuzmich.cz@gmail.com>
2025-10-18 23:13:43 +0200

    Web title, Footer update

--- components/footer.tsx
+++ components/footer.tsx
@@ -1,12 +1,21 @@
-import { APP_NAME } from "@/lib/constants";
+import { APP_NAME } from '@/lib/constants';
+import Link from 'next/link';
 
 const Footer = () => {
   const currentYear = new Date().getFullYear();
 
   return (
     <footer className="border-t">
-      <div className="p-5 flex-center">
-        {currentYear} {APP_NAME}. All rights reserved.
+      <div className="p-5 flex-center text-sm text-muted-foreground">
+        {currentYear} {APP_NAME} &copy; Developed by&nbsp;
+        <Link
+          href="https://github.com/pkuzmich"
+          target="_blank"
+          className="link hover:underline"
+          rel="noopener noreferrer"
+        >
+          Pavlo Kuzmich
+        </Link>
       </div>
     </footer>
   );
