4c24e1d1cdbaa6959d3eb2d85f3254287bf67528
Pavel Kuzmich <pkuzmich.cz@gmail.com>
2025-10-18 14:31:24 +0200

    Sign In and Sign Out Action

--- lib/validators.ts
+++ lib/validators.ts
@@ -1,27 +1,31 @@
-import { z } from "zod";
-import { formatNumberWithDecimal } from "./utils";
+import { z } from 'zod';
+import { formatNumberWithDecimal } from './utils';
 
 const currency = z
   .string()
   .refine(
     (value) => (
       /^\d+(\.\d{2})?$/.test(formatNumberWithDecimal(Number(value))),
-      "Price must have exactly two decimal places"
-    ),
+      'Price must have exactly two decimal places'
+    )
   );
 
 // Schema for inserting products
 export const insertProductSchema = z.object({
-  name: z.string().min(3, "Name must be at least 3 characters long"),
-  slug: z.string().min(3, "Slug must be at least 3 characters long"),
-  category: z.string().min(3, "Category must be at least 3 characters long"),
-  brand: z.string().min(3, "Brand must be at least 3 characters long"),
-  description: z
-    .string()
-    .min(3, "Description must be at least 3 characters long"),
+  name: z.string().min(3, 'Name must be at least 3 characters long'),
+  slug: z.string().min(3, 'Slug must be at least 3 characters long'),
+  category: z.string().min(3, 'Category must be at least 3 characters long'),
+  brand: z.string().min(3, 'Brand must be at least 3 characters long'),
+  description: z.string().min(3, 'Description must be at least 3 characters long'),
   stock: z.coerce.number(),
-  images: z.array(z.string().min(1, "Product must have at least one image")),
+  images: z.array(z.string().min(1, 'Product must have at least one image')),
   isFeatured: z.boolean(),
   banner: z.string().nullable(),
-  price: currency,
+  price: currency
+});
+
+// Schema for signing users in
+export const signInFormSchema = z.object({
+  email: z.email('Invalid email address'),
+  password: z.string().min(5, 'Password must be at least 5 characters long')
 });
