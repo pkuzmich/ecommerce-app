41eff72664143151dd8ef433a2d23c9c9ac37233
Pavel Kuzmich <pkuzmich.cz@gmail.com>
2025-11-16 21:11:51 +0100

    Sign Up Zos Schema

--- lib/actions/user.actions.ts
+++ lib/actions/user.actions.ts
@@ -1,8 +1,10 @@
 'use server';
 
-import { signInFormSchema } from '../validators';
+import { signInFormSchema, signUpFormSchema } from '../validators';
 import { signIn, signOut } from '@/auth';
 import { isRedirectError } from 'next/dist/client/components/redirect-error';
+import { prisma } from '@/db/prisma';
+import { hashSync } from 'bcrypt-ts-edge/browser';
 
 // SignIn user with credentials
 export async function signInCredentials(prevState: unknown, formData: FormData) {
@@ -25,4 +27,36 @@ export async function signInCredentials(prevState: unknown, formData: FormData) 
 // Sign user out
 export async function signOutUser() {
   await signOut();
+}
+
+// Sign up user
+export async function signUpUser(prevState: unknown, formData: FormData) {
+  try {
+    const user = signUpFormSchema.parse({
+      name: formData.get('name'),
+      email: formData.get('email'),
+      password: formData.get('password'),
+      confirmPassword: formData.get('confirmPassword')
+    });
+
+    const plainPassword = user.password;
+
+    user.password = await hashSync(user.password, 10);
+
+    await prisma.user.create({
+      data: user
+    });
+
+    await signIn('credentials', {
+      email: user.email,
+      password: plainPassword
+    });
+
+    return { success: true, message: 'Signed up successfully' };
+  } catch (error) {
+    if (isRedirectError(error)) {
+      throw error;
+    }
+    return { success: false, message: 'Failed to sign up' };
+  }
 }
--- lib/validators.ts
+++ lib/validators.ts
@@ -3,12 +3,9 @@ import { formatNumberWithDecimal } from './utils';
 
 const currency = z
   .string()
-  .refine(
-    (value) => (
-      /^\d+(\.\d{2})?$/.test(formatNumberWithDecimal(Number(value))),
-      'Price must have exactly two decimal places'
-    )
-  );
+  .refine((value) => /^\d+(\.\d{2})?$/.test(formatNumberWithDecimal(Number(value))), {
+    message: 'Price must have exactly two decimal places'
+  });
 
 // Schema for inserting products
 export const insertProductSchema = z.object({
@@ -29,3 +26,16 @@ export const signInFormSchema = z.object({
   email: z.email('Invalid email address'),
   password: z.string().min(5, 'Password must be at least 5 characters long')
 });
+
+// Schema for signing users up
+export const signUpFormSchema = z
+  .object({
+    name: z.string().min(3, 'Name must be at least 3 characters long'),
+    email: z.email('Invalid email address'),
+    password: z.string().min(5, 'Password must be at least 5 characters long'),
+    confirmPassword: z.string().min(5, 'Confirm password must be at least 5 characters long')
+  })
+  .refine((data) => data.password === data.confirmPassword, {
+    message: "Passwords don't match",
+    path: ['confirmPassword'] // Error appears on confirmPassword field
+  });
